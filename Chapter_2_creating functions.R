bbdc9a28444970ec58470d7debf9239de89b0640
Golam Mahmudunnabi <mnrabbee@gmail.com>
2026-02-18 21:53:07 +1000

    Function

--- Chapter_2_creating functions.R
+++ Chapter_2_creating functions.R
@@ -11,4 +11,46 @@ fahrenheit_to_celsius <- function (temp_F) {
 }
 
 
-fahrenheit_to_celsius(212)
+celsius_to_kelvin <- function(temp_C) {
+  
+  temp_K <- temp_C + 273.15
+  
+  return(temp_K)
+}
+
+
+fahrenheit_to_kelvin <- function(temp_F) {
+  
+  temp_C <- fahrenheit_to_celsius(temp_F)
+  
+  temp_K <- celsius_to_kelvin(temp_C)
+  
+  return(temp_K)
+}
+
+fahrenheit_to_kelvin(32.0)
+
+celsius_to_kelvin(fahrenheit_to_celsius(32.0))
+
+#creating a function to combine the vector
+
+vec1 <- c("a", "b")
+
+vec2 <- c("****")
+
+
+highlight <- function(vec1, vec2){
+  
+  vec <- c(vec1, vec2)
+  
+  return(vec)
+}
+
+highlight(vec1, vec2)
+
+
+
+
+
+
+
